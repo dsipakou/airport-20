cd921d9a79ddc820092201255d3e97dfcc1e5a87
Denis Sipakov <denis.sipakov@pandadoc.com>
2019-04-09 18:16:36 +0300

    Remove interface

--- app/src/main/java/com/example/airport20/presentation/flightdetails/DetailsFragment.kt
+++ app/src/main/java/com/example/airport20/presentation/flightdetails/DetailsFragment.kt
@@ -41,15 +41,6 @@ class DetailsFragment : Fragment() {
         listener?.onFragmentInteraction(uri)
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
-        }
-    }
-
     override fun onDetach() {
         super.onDetach()
         listener = null
