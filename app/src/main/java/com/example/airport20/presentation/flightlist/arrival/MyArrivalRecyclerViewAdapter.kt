c54090ceaf3356e4bb42c103bdcb0ae70c2862ca
Dzianis Sipakou <d.sipakou@gmail.com>
2019-04-19 10:29:13 +0300

    Move adapter

--- app/src/main/java/com/example/airport20/presentation/flightlist/arrival/MyArrivalRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/airport20/presentation/flightlist/arrival/MyArrivalRecyclerViewAdapter.kt
@@ -1,11 +1,14 @@
-package com.example.airport20.presentation.flightlist
+package com.example.airport20.presentation.flightlist.arrival
 
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil.calculateDiff
 import androidx.recyclerview.widget.RecyclerView
 import com.example.airport20.R
+import com.example.airport20.domain.Arrival
+import com.example.airport20.domain.FlightManager
 import com.example.airport20.dummy.DummyContent
 
 
@@ -13,13 +16,13 @@ import com.example.airport20.dummy.DummyContent.DummyItem
 
 import kotlinx.android.synthetic.main.fragment_arrival.view.*
 
-typealias ClickListener = (DummyItem) -> Unit
+typealias ClickListener = (Arrival) -> Unit
 
 class MyArrivalRecyclerViewAdapter(
     private val clickListener: ClickListener
 ) : RecyclerView.Adapter<MyArrivalRecyclerViewAdapter.ViewHolder>() {
 
-    private var flightList = DummyContent.ITEMS
+    private var flightList: List<Arrival> = emptyList<Arrival>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -34,7 +37,11 @@ class MyArrivalRecyclerViewAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = flightList[position]
         holder.mIdView.text = item.id.toString()
-        holder.mContentView.text = item.content
+        holder.mContentView.text = item.city
+    }
+
+    fun updateList(flightList: List<Arrival>) {
+        this.flightList = flightList
     }
 
     override fun getItemCount(): Int = flightList.size
