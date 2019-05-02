e2d54f0d2b711b0bab3c251dcab4af3dce253b33
Dzianis Sipakou <d.sipakou@gmail.com>
2019-05-02 13:40:37 +0300

    Fix back button action

--- app/src/main/java/com/example/airport20/MainActivity.kt
+++ app/src/main/java/com/example/airport20/MainActivity.kt
@@ -7,31 +7,20 @@ import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
-import com.google.android.material.navigation.NavigationView
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
-import java.util.*
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        val toggle = ActionBarDrawerToggle(
-            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
-        )
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        nav_view.setNavigationItemSelectedListener {
-            it.isChecked = true
-            drawer_layout.closeDrawers()
-            true
-        }
         setupNavigation()
     }
 
@@ -59,38 +48,50 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
-        when (item.itemId) {
-            R.id.now_timetable -> {
-
-            }
-            R.id.today_timetable -> {
-                // Handle the camera action
-            }
-            R.id.yesterday_timetable -> {
-
-            }
-            R.id.tomorrow_timetable -> {
-
-            }
-            R.id.nav_manage -> {
-
-            }
-        }
-
-        drawer_layout.closeDrawer(GravityCompat.START)
-        return true
-    }
+//    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+//        // Handle navigation view item clicks here.
+//        when (item.itemId) {
+////            R.id.now_timetable -> {
+////
+////            }
+////            R.id.today_timetable -> {
+////                // Handle the camera action
+////            }
+////            R.id.yesterday_timetable -> {
+////
+////            }
+////            R.id.tomorrow_timetable -> {
+////
+////            }
+//            R.id.nav_manage -> {
+//                drawer_layout.closeDrawers()
+//                findNavController(R.id.myNavHostFragment).navigate(actionTabsFragment2ToSettingsFragment())
+//                return true
+//            }
+//        }
+//
+//        drawer_layout.closeDrawer(GravityCompat.START)
+//        return true
+//    }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.myNavHostFragment)
-        return NavigationUI.navigateUp(navController, drawer_layout)
+        return navigateUp(findNavController(this, R.id.myNavHostFragment), drawer_layout)
     }
 
     private fun setupNavigation() {
-        val navController = findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
-        NavigationUI.setupWithNavController(nav_view, navController)
+        val navController = findNavController(this, R.id.myNavHostFragment)
+
+        // Update action bar to reflect navigation
+        setupActionBarWithNavController(this, navController, drawer_layout)
+
+        // Handle nav drawer item clicks
+        nav_view.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            drawer_layout.closeDrawers()
+            true
+        }
+
+        // Tie nav graph to items in nav drawer
+        setupWithNavController(nav_view, navController)
     }
 }
