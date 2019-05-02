d66220eef896dfd9608fedb1fcb320050cfda572
Dzianis Sipakou <d.sipakou@gmail.com>
2019-05-02 14:16:12 +0300

    Add settings fragment

--- app/src/main/java/com/example/airport20/presentation/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/airport20/presentation/settings/SettingsFragment.kt
@@ -2,23 +2,14 @@ package com.example.airport20.presentation.settings
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 
 import com.example.airport20.R
 
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
     }
-
-
 }
