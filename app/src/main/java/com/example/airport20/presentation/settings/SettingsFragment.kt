252dda33515c12e247543f71f94fc26a344c15a0
Dzianis Sipakou <d.sipakou@gmail.com>
2019-05-02 16:44:38 +0300

    Add preference listener

--- app/src/main/java/com/example/airport20/presentation/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/airport20/presentation/settings/SettingsFragment.kt
@@ -2,6 +2,8 @@ package com.example.airport20.presentation.settings
 
 
 import android.os.Bundle
+import android.util.Log
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 
 import com.example.airport20.R
@@ -11,5 +13,12 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+        val pref = findPreference(getString(R.string.pref_key_language))
+        pref.setOnPreferenceChangeListener { preference, newValue ->
+            if (preference is ListPreference) {
+                Log.i("Preferences", newValue.toString())
+            }
+            true
+        }
     }
 }
