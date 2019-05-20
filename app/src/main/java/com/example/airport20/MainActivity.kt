c0dffec9b656a6b3a7cd1fcc6c35b4826f3f830a
Dzianis Sipakou <d.sipakou@gmail.com>
2019-05-20 11:17:27 +0300

    Refactoring

--- app/src/main/java/com/example/airport20/MainActivity.kt
+++ app/src/main/java/com/example/airport20/MainActivity.kt
@@ -47,6 +47,10 @@ class MainActivity : AppCompatActivity() {
     fun setLanguage(language: String) {
         LocalHelper().setLocal(this, language)
         this.recreate()
+        refresh()
+    }
+
+    private fun refresh() {
         ArrivalViewModel().refresh()
         DepartureViewModel().refresh()
     }
@@ -56,22 +60,6 @@ class MainActivity : AppCompatActivity() {
             drawer_layout.closeDrawer(GravityCompat.START)
         } else {
             super.onBackPressed()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
         }
     }
 
