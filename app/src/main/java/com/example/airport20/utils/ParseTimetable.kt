0b4115c604e830c4e0925f756cc58e3cf29f81db
Dzianis Sipakou <d.sipakou@gmail.com>
2020-02-28 12:09:54 +0300

    Fixing arrival

--- app/src/main/java/com/example/airport20/utils/ParseTimetable.kt
+++ app/src/main/java/com/example/airport20/utils/ParseTimetable.kt
@@ -2,7 +2,6 @@ package com.example.airport20.utils
 
 import android.util.Log
 import com.example.airport20.domain.*
-import com.google.gson.Gson
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
@@ -10,40 +9,43 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
+import org.json.JSONArray
+import org.json.JSONObject
 import java.util.*
 
 class ParseTimetable {
     suspend fun getArrivals() = withContext(Dispatchers.IO) {
         launch {
             try {
-                val url = getUrl(FlightType.ARRIVAL)
-                val period = getPeriod()
-                val okHttpClient = OkHttpClient()
-                val parsedResponse = parseResponse(okHttpClient.newCall(createRequest(url)).execute())
-                val arrival = Gson().fromJson(parsedResponse, Arrival::class.java)
-                val document = Jsoup.connect(url).get()
-                val tr = document.select("table#fl-arrival tbody tr$period")
-                tr.forEach {
-                    val tds = it.select("td")
-                    if (tds.size > 0) {
-                        val actualTime: AirportTime
-                        val status: Status
+                fun setFlights(entity: JSONObject) {
+                    try {
+//                    val status: Status
+                        val statusObject = JSONObject(entity.getString("status"))
+                        val status = statusObject.getString("id")
                         val regex = Regex("([A-Z- ]+)\\s+(expected|departure)[a-z ]+(\\d+:\\d+)")
-                        val res = regex.find(tds[6].text())
-                        if (res != null) {
-                            actualTime = parseTime(res.groups[3]!!.value)
-                            status = Status.fromString(res.groups[1]!!.value)
-                        } else {
-                            actualTime = parseTime(tds[2].text())
-                            status = Status.fromString(tds[6].text())
-                        }
+                        val actualTime: AirportTime = Dates.getAirportTime(entity.getString("plan"))
+//                    val res = regex.find(arrival.text())
+//                    if (res != null) {
+//                        actualTime = parseTime(res.groups[3]!!.value)
+//                        status = Status.fromString(res.groups[1]!!.value)
+//                    } else {
+//                        actualTime = parseTime(arrival[2].text())
+//                        status = Status.fromString(arrival[6].text())
+//                    }
                         val id = UUID.randomUUID().toString()
-                        val company = tds[0].text()
-                        val code = tds[3].text()
-                        val gate = tds[5].text()
-                        val expectedTime = parseTime(tds[1].text())
-                        val city = tds[4].text()
-                        val cityCode = sanitizeString(tds[4].text())
+//                    val id = JSONObject(entity.getString("flight_id"))
+                        val companyObject = JSONObject(entity.getString("airline"))
+                        val company = companyObject.getString("title")
+                        val code = entity.getString("flight")
+                        val gate = entity.getString("gate")
+                        val cityObject = JSONObject(entity.getString("airport"))
+                        val city = cityObject.getString("title")
+                        var expectedTime: AirportTime = AirportTime(null, null)
+                        if (entity.getString("fact") != "null") {
+                            expectedTime = Dates.getAirportTime(entity.getString("fact"))
+                        }
+//                    val expectedTime = parseTime(arrival[1].text())
+                        val cityCode = sanitizeString(city)
                         if (cityCode.isNotEmpty()) {
                             FlightManager.addArrival(
                                 Arrival(
@@ -59,10 +61,48 @@ class ParseTimetable {
                                     registrationDesk = "",
                                     city = city,
                                     cityCode = cityCode,
-                                    status = status,
+                                    status = Status.fromString(status),
                                     imageUrl = ""
                                 )
                             )
+                        }
+                    } catch (e: Exception) {
+                        Log.e("Parse flight", "Can't parse flight: " + e.stackTrace)
+                    }
+                }
+                val url = getUrl(FlightType.ARRIVAL)
+                val period = getPeriod()
+                val okHttpClient = OkHttpClient()
+                val parsedResponse = parseResponse(okHttpClient.newCall(createRequest(url)).execute())
+                val arrivals = JSONArray(parsedResponse)
+//                val document = Jsoup.connect(url).get()
+//                val tr = document.select("table#fl-arrival tbody tr$period")
+                for (i in 0 until arrivals.length()) {
+                    if (i > 85) {
+                        Log.i("Hello", "there")
+                    }
+                    val entity = JSONObject(arrivals.get(i).toString())
+                    val flightTime: Date = Dates.parseTime(entity.getString("plan"))
+                    when (FlightManager.getPeriod()) {
+                        TimeRange.NOW -> {
+                            if (Date().addHours(-1) < flightTime && flightTime < Date().addHours(2)) {
+                                setFlights(entity)
+                            }
+                        }
+                        TimeRange.YESTERDAY -> {
+                            if (flightTime.isYesterday()) {
+                                setFlights(entity)
+                            }
+                        }
+                        TimeRange.TODAY -> {
+                            if (flightTime.isToday()) {
+                                setFlights(entity)
+                            }
+                        }
+                        TimeRange.TOMORROW -> {
+                            if (flightTime.isTomorrow()) {
+                                setFlights(entity)
+                            }
                         }
                     }
                 }
@@ -88,7 +128,7 @@ class ParseTimetable {
                         val regex = Regex("([A-Z- ]+)\\s+(expected|departure)[a-z ]+(\\d+:\\d+)")
                         val res = regex.find(tds[6].text())
                         if (res != null) {
-                            actualTime = parseTime(res.groups[3]!!.value)
+                            actualTime = Dates.getAirportTime(res.groups[3]!!.value)
                             status = Status.fromString(res.groups[1]!!.value)
                         } else {
                             actualTime = AirportTime(null, null)
@@ -98,7 +138,7 @@ class ParseTimetable {
                         val company = tds[0].text()
                         val code = tds[2].text()
                         val gate = tds[5].text()
-                        val expectedTime = parseTime(tds[1].text())
+                        val expectedTime = Dates.getAirportTime(tds[1].text())
                         val registrationDesk = tds[4].text()
                         val city = tds[3].text()
                         val cityCode = sanitizeString(tds[3].text())
--- app/src/main/java/com/example/airport20/utils/TimeUtils.kt
+++ app/src/main/java/com/example/airport20/utils/TimeUtils.kt
@@ -1,11 +1,115 @@
 package com.example.airport20.utils
 
 import com.example.airport20.domain.AirportTime
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
-fun parseTime(time: String): AirportTime {
-    val re = Regex("(?:(\\d+.\\d+) )?(\\d+:\\d+)\$")
-    val formatter = re.find(time)
-    val airportTime: AirportTime
-    airportTime = AirportTime(formatter?.groups?.get(1)?.value, formatter?.groups?.get(2)?.value)
-    return airportTime
+
+object Dates {
+    val MINSK_TIME_ZONE = TimeZone.getTimeZone("GMT+3")
+
+    fun from(day: Date, time: Date, timeZone: TimeZone? = null): Date {
+        val dateComponents = Calendar.getInstance(timeZone ?: TimeZone.getDefault())
+        dateComponents.timeInMillis = day.time
+
+        val timeComponents = Calendar.getInstance(timeZone ?: TimeZone.getDefault())
+        timeComponents.timeInMillis = time.time
+
+        dateComponents.set(Calendar.HOUR_OF_DAY, timeComponents.get(Calendar.HOUR_OF_DAY))
+        dateComponents.set(Calendar.MINUTE, timeComponents.get(Calendar.MINUTE))
+        dateComponents.set(Calendar.SECOND, timeComponents.get(Calendar.SECOND))
+        dateComponents.set(Calendar.MILLISECOND, timeComponents.get(Calendar.MILLISECOND))
+
+        return dateComponents.time
+    }
+
+    fun parseTime(timeStr: String): Date {
+//    val datetime = OffsetDateTime.parse(timeStr)
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+//        val date = SimpleDateFormat("dd-MM")
+//        val time = SimpleDateFormat("HH:mm")
+        return dateFormat.parse(timeStr)
+//        val now = dateFormat.format(Date())
+//        val cal = Calendar.getInstance(MINSK_TIME_ZONE)
+//
+//        val re = Regex("(?:(\\d+.\\d+) )?(\\d+:\\d+)\$")
+//        val formatter = re.find(timeStr)
+//        val airportTime: AirportTime
+//        airportTime = AirportTime(date.format(formatedDateTime), time.format(formatedDateTime))
+//        return airportTime
+    }
+
+    fun getAirportTime(timeStr: String): AirportTime {
+        val datetime = parseTime(timeStr)
+        val date = SimpleDateFormat("dd-MM")
+        val time = SimpleDateFormat("HH:mm")
+        return AirportTime(date.format(datetime), time.format(datetime))
+    }
+
+    fun today(timeZone: TimeZone? = null): Date =
+        Date().midnight(timeZone = timeZone)
+
+
+    fun tomorrow(timeZone: TimeZone? = null): Date =
+        today(timeZone = timeZone).addDays(1)
+
+
+    fun yesterday(timeZone: TimeZone? = null): Date =
+        today(timeZone = timeZone).addDays(-1)
+
+}
+
+fun Date.midnight(timeZone: TimeZone? = null): Date {
+    val calendar = Calendar.getInstance(timeZone ?: TimeZone.getDefault())
+    calendar.timeInMillis = this.time
+    calendar.set(Calendar.HOUR_OF_DAY, 0)
+    calendar.set(Calendar.MINUTE, 0)
+    calendar.set(Calendar.SECOND, 0)
+    calendar.set(Calendar.MILLISECOND, 0)
+
+    return calendar.time
+}
+
+fun Date.addDays(days: Int): Date {
+    val calendar = Calendar.getInstance()
+    calendar.timeInMillis = this.time
+    calendar.add(Calendar.DAY_OF_YEAR, days)
+    return calendar.time
+}
+
+fun Date.addHours(hours: Int): Date {
+    val calendar = Calendar.getInstance()
+    calendar.timeInMillis = this.time
+    calendar.add(Calendar.HOUR_OF_DAY, hours)
+    return calendar.time
+}
+
+fun Date.addMinutes(minutes: Int): Date {
+    val calendar = Calendar.getInstance()
+    calendar.timeInMillis = this.time
+    calendar.add(Calendar.MINUTE, minutes)
+    return calendar.time
+}
+
+fun Date.isToday(timeZone: TimeZone? = null): Boolean =
+    isSameDay(Dates.today(timeZone = timeZone), timeZone = timeZone)
+
+fun Date.isTomorrow(timeZone: TimeZone? = null): Boolean =
+    isSameDay(Dates.tomorrow(timeZone = timeZone), timeZone = timeZone)
+
+fun Date.isYesterday(timeZone: TimeZone? = null): Boolean =
+    isSameDay(Dates.yesterday(timeZone = timeZone), timeZone = timeZone)
+
+fun Date.isSameDay(day: Date, timeZone: TimeZone? = null): Boolean {
+    val currentDate = Calendar.getInstance(timeZone ?: TimeZone.getDefault())
+    currentDate.timeInMillis = this.time
+
+    val anotherDay = Calendar.getInstance(timeZone ?: TimeZone.getDefault())
+    anotherDay.timeInMillis = day.time
+
+    val sameDay = currentDate.get(Calendar.DAY_OF_YEAR) == anotherDay.get(Calendar.DAY_OF_YEAR)
+    val sameYear = currentDate.get(Calendar.YEAR) == anotherDay.get(Calendar.YEAR)
+
+    return sameDay && sameYear
 }
